172f22458b7eb916163b6da514165c5c259cce82
devnoman01 <devnoman01@gmail.com>
2022-02-17 15:28:06 +0600

    total expense and balance calculation complete

--- js/app.js
+++ js/app.js
@@ -1,24 +1,46 @@
 function getInputValue(id) {
     const inputFieldValue = document.getElementById(id);
-    const inputValue = parseInt(inputFieldValue.value);
+    var inputValue = parseInt(inputFieldValue.value);
+    return inputValue;
 
-    if(inputValue < 0){
-        document.getElementById(id).style.border = "2px solid red";
-        if(id == 'income-input'){
-            document.getElementById('income-alert').style.display = "block";
-        }
-    }
-    else{
-        return inputValue;
-    }
-
+    // document.getElementById(id).style.border = "2px solid red";
 }
 
 function incomeCalculation() {
     const incomeAmount = getInputValue('income-input');
+    return incomeAmount;
 }
 
-function expenseCalc() {
+function expenseCalculation() {
+    const foodExpense = getInputValue('food-expense');
+    const rentxpense = getInputValue('rent-expense');
+    const clothExpense = getInputValue('cloth-expense');
+    const totalExpense = foodExpense + rentxpense + clothExpense;
+    document.getElementById('total-expense').innerText = totalExpense;
+    return totalExpense;
+}
+
+function balanceCalculation() {
     const income = incomeCalculation();
+    const expense = expenseCalculation();
+
+    if(expense > income){
+        document.getElementById('expense-alert').style.display = "block";
+    }
+    else{
+        document.getElementById('expense-alert').style.display = "none";
+        const balanceAmount = incomeCalculation() - expenseCalculation();
+        document.getElementById('balance-amount').innerText = balanceAmount;
+        return balanceAmount;
+    }
+}
+
+
+
+function calculateBalance() {
+    
+    expenseCalculation(); 
+    balanceCalculation(); 
+    
 }
 
