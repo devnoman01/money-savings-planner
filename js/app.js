4da1372b2ec726faca9deae2ad299b5b7e483dd0
devnoman01 <devnoman01@gmail.com>
2022-02-17 19:17:04 +0600

    all calculation and error handling complete

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 // input field value processing
 function getInputValue(id) {
     const inputFieldValue = document.getElementById(id);
-    var inputValue = parseInt(inputFieldValue.value);
-    return inputValue;
 
-    // document.getElementById(id).style.border = "2px solid red";
+    if((inputFieldValue.value).length == 0){
+        document.getElementById(id + '-alert').style.display = "block";
+        return;
+    }
+    else{
+        var inputValue = parseFloat(inputFieldValue.value);
+
+        if(inputValue>=0){
+            document.getElementById(id + '-alert').style.display = "none";
+            return inputValue;
+        }
+        else{
+            document.getElementById(id + '-alert').style.display = "block";
+            document.getElementById(id + '-alert').innerText = "Negative input not supported";
+            return;
+        }
+    }
 }
 
 // income calculation
@@ -19,33 +33,51 @@ function expenseCalculation() {
     const rentxpense = getInputValue('rent-expense');
     const clothExpense = getInputValue('cloth-expense');
     const totalExpense = foodExpense + rentxpense + clothExpense;
-    document.getElementById('total-expense').innerText = totalExpense;
     return totalExpense;
 }
 
 // balance calculation
 function balanceCalculation() {
+    console.log("balanceCalc function called");
     const income = incomeCalculation();
     const expense = expenseCalculation();
 
     // checking if expense is greater than income
-    if(expense > income){
-        document.getElementById('expense-alert').style.display = "block";
-    }
-    else{
-        document.getElementById('expense-alert').style.display = "none";
-        const balanceAmount = incomeCalculation() - expenseCalculation();
+    if(expense <= income){
+        document.getElementById('total-expense').innerText = expense;
+        document.getElementById('expense-error-alert').style.display = "none";
+        const balanceAmount = income - expense;
         document.getElementById('balance-amount').innerText = balanceAmount;
         return balanceAmount;
     }
+    else if(expense > income){
+        document.getElementById('expense-error-alert').style.display = "block";
+    }
 }
 
+// saving calculation
+function savingCalculation() {
+    const savingPercentage = getInputValue('saving-input');
+    const savingAmount = (incomeCalculation() / 100) * savingPercentage;
+    const balanceAmount = balanceCalculation();
+    if (savingAmount > balanceAmount) {
+        document.getElementById('saving-exceed-alert').style.display = "block";
+    }
+    else if(savingAmount < balanceAmount){
+        document.getElementById('saving-exceed-alert').style.display = "none";
+        document.getElementById('saving-amount').innerText = savingAmount;
+        document.getElementById('remaining-balance').innerText = balanceCalculation() - savingAmount;
+        return savingAmount;
+    }
 
+}
 // calculate button function
 function calculateBalance() {
-    expenseCalculation(); 
     balanceCalculation(); 
 }
 
-
+// save button function
+function calculateSaving() {
+    savingCalculation();
+}
 
